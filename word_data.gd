e3c0afc5d42302e624d084e0209d326c2da8bd5f
jkarenko <juho.karenko@gmail.com>
2024-11-20 15:02:08 +0200

    refactor(data): Refactor word_data.gd to follow the new schema

--- word_data.gd
+++ word_data.gd
@@ -2,7 +2,6 @@ extends Node
 
 static var WORD_CATEGORIES = {}
 static var WORD_RELATIONSHIPS = {}
-static var _processed_relationships = {}
 
 func _init():
     # Load the JSON file
@@ -15,49 +14,35 @@ func _init():
             WORD_CATEGORIES = data["WORD_CATEGORIES"]
             WORD_RELATIONSHIPS = data["WORD_RELATIONSHIPS"]
         file.close()
-    # Process relationships to make them bidirectional
-    _processed_relationships = WORD_RELATIONSHIPS.duplicate(true)
-    for word1 in WORD_RELATIONSHIPS:
-        for word2 in WORD_RELATIONSHIPS[word1]:
-            if not word2 in _processed_relationships:
-                _processed_relationships[word2] = {}
-            _processed_relationships[word2][word1] = WORD_RELATIONSHIPS[word1][word2]
 
 static func get_relationship_score(word1: String, word2: String) -> Dictionary:
     var instance = Engine.get_main_loop().get_root().get_node_or_null("/root/WordData")
     if instance == null:
         instance = load("res://word_data.gd").new()
     
-    var score = 0
-    var type = ""
-    
-    if word1 in _processed_relationships and word2 in _processed_relationships[word1]:
-        type = _processed_relationships[word1][word2]
-    else:
-        var common_words = []
-        if word1 in _processed_relationships and word2 in _processed_relationships:
-            for related_word in _processed_relationships[word1].keys():
-                if related_word in _processed_relationships[word2]:
-                    common_words.append(related_word)
-        
-        if common_words.size() > 0:
-            type = "common"
-    
-    match type:
-        "compound": score = 40
-        "strong": score = 30
-        "thematic": score = 20
-        "common": score = 10
+    # Search through relationships to find connections
+    for row_data in WORD_RELATIONSHIPS.values():
+        for word_data in row_data:
+            if word_data["word"] == word1 and word2 in word_data["related_to"]:
+                return {
+                    "score": 30,  # Default score for direct relationships
+                    "type": word_data["relationship"]
+                }
+            elif word_data["word"] == word2 and word1 in word_data["related_to"]:
+                return {
+                    "score": 30,  # Default score for direct relationships
+                    "type": word_data["relationship"]
+                }
     
     return {
-        "score": score,
-        "type": type
+        "score": 0,
+        "type": ""
     }
 
 static func get_all_words() -> Array:
     var all_words = []
-    for category in WORD_CATEGORIES.values():
-        all_words.append_array(category)
+    for row_key in WORD_CATEGORIES.keys():
+        all_words.append_array(WORD_CATEGORIES[row_key])
     return all_words
 
 static func get_words_from_categories(num_categories: int) -> Array:
